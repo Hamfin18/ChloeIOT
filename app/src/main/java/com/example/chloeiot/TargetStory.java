f4eb83ce79094aaf0b4f4d661aa0a2413c6285b0
Hamfin <fistilham@gmail.com>
2021-04-21 08:49:24 +0700

    History / notification selesai(4).

--- app/src/main/java/com/example/chloeiot/TargetStory.java
+++ app/src/main/java/com/example/chloeiot/TargetStory.java
@@ -1,11 +1,14 @@
 package com.example.chloeiot;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.text.Html;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.google.firebase.database.DataSnapshot;
@@ -13,7 +16,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.squareup.picasso.Picasso;
 
 public class TargetStory extends AppCompatActivity {
 
@@ -25,6 +27,9 @@ public class TargetStory extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_target_story);
+
+        getSupportActionBar().setTitle(Html.fromHtml("<font color='#000000'></font>")); //SET TOP NAV TITLE
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //bikin tombol back
 
         judulnya=(TextView)findViewById(R.id.judulnya);
         isinya=(TextView)findViewById(R.id.isinya);
@@ -60,4 +65,20 @@ public class TargetStory extends AppCompatActivity {
     public  void jadi(String foto){
         Glide.with(this).load(foto).into(fotonya);
     }
+
+    public boolean onOptionsItemSelected(MenuItem item) {   // ADD BACK BUTTON
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {     // ADD BACK BUTTON
+        return true;
+    }
+
 }
+
+
