58532179bbffe6755ac97c3aa147ffb924345799
Hamfin <fistilham@gmail.com>
2021-07-26 11:43:08 +0700

    fix icon

--- app/src/main/java/com/example/chloeiot/TaskActivity.java
+++ app/src/main/java/com/example/chloeiot/TaskActivity.java
@@ -1,6 +1,9 @@
 package com.example.chloeiot;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.Menu;
@@ -13,6 +16,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -21,7 +25,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class TaskActivity extends AppCompatActivity {
-
+        private NotificationManagerCompat notificationManager;
         private Button buttCekSuhu,buttSiram ;
         private ProgressBar progBar;
         private TextView angka_progress,soil_moisture;
@@ -41,11 +45,20 @@ public class TaskActivity extends AppCompatActivity {
         soil_moisture =(TextView)findViewById(R.id.soil_moisture);
         buttSiram=(Button)findViewById(R.id.buttSiram);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel =new NotificationChannel("my notification","my notification", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+//        notifSuhu();
+
         buttCekSuhu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {                                                   //ON CLICK BUTTON SCAN
                 angka_progress.setText("Loading");
                 setKata();
+
             }
 
         });
@@ -88,6 +101,23 @@ public class TaskActivity extends AppCompatActivity {
         });
             }
 
+//        public void notifSuhu(){
+//        reff = FirebaseDatabase.getInstance().getReference().child("DRealtime");
+//        reff.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                String Suhu = snapshot.child("humidity").getValue().toString();
+//
+//
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+//        }
 
         public void setKata(){                                                                          //AMBIL DATA DARI FIREBASE
             reff= FirebaseDatabase.getInstance().getReference().child("DRealtime");
@@ -96,6 +126,15 @@ public class TaskActivity extends AppCompatActivity {
                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
                     String Suhu=dataSnapshot.child("humidity").getValue().toString();
+//                    if (Suhu.equals("90")){
+//                        NotificationCompat.Builder builder =new NotificationCompat.Builder(TaskActivity.this,"my notification");
+//                        builder.setContentTitle("Text");
+//                        builder.setContentText("Hayo siram");
+//                        builder.setSmallIcon(R.drawable.notification);
+//                        builder.setAutoCancel(true);
+//                    }
+
+
                     progBar.setProgress(Integer.valueOf(Suhu));
                     angka_progress.setText(Suhu+"%");
                     soil_moisture.setText("Soil Moisture");
